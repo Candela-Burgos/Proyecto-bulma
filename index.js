fc4189840982fe1257677142f790fe5ce0f14ad3
Candela <candelaburgos10@yahoo.com>
2022-01-28 21:54:53 -0300

    agregue imagenes, cards, modal, comentarios, grid.

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const hamburIcon=document.querySelector('#burger')
-const navbarmenu=document.querySelector('#nav-links')
+// VARIABLES GLOBALES
 
-hamburIcon.addEventListener('click',()=>{
-    navbarmenu.classList.toggle('is-active')
+const burgerIcon = document.querySelector('#burger');
+const navBarMenu = document.querySelector('#nav-links');
 
-})
+// EVENTOS
 
+const activarNavBarMenu = () => {
+    navBarMenu.classList.toggle('is-active')//toggle= cuando se aprieta, se agrega y cuando se aprieta nuevamente se saca, y asi susesivamente.
+}
+
+burgerIcon.addEventListener('click', activarNavBarMenu)
+
+// MODAL    
+
+// VARIABLES GLOBALES
+const btnRegister = document.querySelector('#btn-register');
+const modalRegister = document.querySelector('.modal');
+const modalBg = document.querySelector('.modal-background');
+
+// EVENTOS
+
+const abrirModal = () => {
+    modalRegister.classList.add('is-active')
+}
+
+btnRegister.addEventListener('click', abrirModal);
+
+const cerrarModal = () => {
+    modalRegister.classList.remove('is-active')
+}
+
+modalBg.addEventListener('click', cerrarModal);
